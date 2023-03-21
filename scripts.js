f9c9b850bbfffc32b70e19a2488eff2905228cc6
Marlos11 <marlos-gama@hotmail.com>
2023-03-21 09:25:17 -0300

    finalizado

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const changeColor = (color)=>{
+
+    let circle = document.querySelector(".circulo")
+    circle.style.background = color 
+} 
+
+
+const changeImg = (imagem)=>{
+
+    let imgColor = document.querySelector(".iphone")
+    imgColor.src = imagem
+}
